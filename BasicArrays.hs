36fda7c42a9f39e93cbe83487a68ce4904035432
David Fontenot <fontenod@onid.oregonstate.edu>
2014-02-09 13:22:18 -0800

    added array powers

--- BasicArrays.hs
+++ BasicArrays.hs
@@ -3,6 +3,7 @@
 module BasicArrays where
 import Prelude
 import Data.Array
+import Control.Monad
 
 -- array (lowbound, highbound) [(idx, val)]
 squares = array (1,100) [(i, i*i) | i <- [1..100]]
@@ -42,3 +43,32 @@ matMult a b = if n1 /= n2 then Nothing else Just $ array ((1,1),(r,c))
         (_,(r,n1)) = bounds a
         (_,(n2,c)) = bounds b
         rowByCol' a b i j = dot (getRow a i) (getCol b j)
+
+-- iterative method
+matPower :: (Num a) => Array (Int, Int) a -> Int -> Maybe (Array (Int, Int) a)
+matPower m i = matPower' (Just m) (Just m) i
+         where
+         matPower' maccum _ i | i == 1 = maccum
+         matPower' maccum m i | otherwise = matPower' (join $ liftM2 matMult maccum m) m (i-1)
+
+-- get the diagonal row of the identity matrix
+diagOfId4 = (liftM getDiag) (matMult (idMatrix 4) (idMatrix 4))
+
+-- basic arrays
+-- TODO: use some more diverse matricies to check results
+mat1 = array ((1,1),(2,2)) [((1,1),1), ((1,2),2), ((2,1),3), ((2,2),4)]
+mat2 = array ((1,1),(2,2)) [((1,1),5), ((1,2),6), ((2,1),7), ((2,2),8)]
+
+-- answers
+multAns = Just (array ((1,1),(2,2)) [((1,1),19), ((1,2),22), ((2,1),43), ((2,2),50)])
+mat1SquaredAns = Just (array ((1,1),(2,2)) [((1,1),7), ((1,2),10), ((2,1),15), ((2,2),22)])
+mat2QuintedAns = Just (array ((1,1),(2,2)) [((1,1),152393), ((1,2),177474), ((2,1),207053), ((2,2),241130)])
+
+-- operations
+matrixproduct = matMult mat1 mat2
+mat1Squared = matPower mat1 2
+mat2Quinted = matPower mat2 5
+
+-- basic check to see if the math is right
+multCorrect = multAns == matrixproduct
+powerCorrect = mat1Squared == mat1SquaredAns && mat2Quinted == mat2QuintedAns && (matPower mat1 1) == (Just mat1)
